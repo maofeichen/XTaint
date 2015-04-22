ab5ee77dcf9cc3a90cf72834decbfe651fc13d82
mchen <mchen2016@gmail.com>
2015-04-22 11:41:09 -0400

    fin creating buffer set

--- OfflineAnalysis/Search_Avalanche_Effect/SearchAvalanche.py
+++ OfflineAnalysis/Search_Avalanche_Effect/SearchAvalanche.py
@@ -21,14 +21,15 @@ CALL = "14"   # 14 is CALL instruction
 RET = "18"    # 18 is RET instruction
 
 xtaint_log_list = []
+xtaint_buffer_set_list = []
 
 def preprocess():
     """pre-process XTaint log.
     
-    input:
-        path of XTaint log
-    output:
-        generate a list that contains all XTaint log records, such as:
+    Args:
+        global constant - path of XTaint log
+    Returns:
+        save in the list that contains all XTaint log records, such as:
             [{}, {}, {},...]
             each sub-dictonary is either normal record or CALL or RET record,
             form as dictionary:
@@ -40,20 +41,19 @@ def preprocess():
     """
     with open(XTAINT_LOG_DIR + XTAINT_LOG_FILE, 'r') as xt_log_fp:
         for line in xt_log_fp:
-            if line[:2] == CALL:
-                words = line.split()
+            words = line.split()
+            if words[0] == CALL:
                 """
-                example:
+                example words:
                     list: ['14', '0', '0', 'CALL', 'level:-215']
                 needs to save func_addr ('0') and 'level:-215'
                 """
                 call = {}
                 call['CALL'] = {'func_addr':words[1], 'level':words[4]}
                 xtaint_log_list.append(call)
-            elif line[:2] == RET:
-                words = line.split()
+            elif words[0] == RET:
                 """
-                example: 
+                example words: 
                     list: ['18', '0', '0', 'RET', 'level:-1']
                 only needs to save "RET" and "level:-1"
                 """
@@ -62,22 +62,82 @@ def preprocess():
                 xtaint_log_list.append(ret)
             else:
                 """normal record"""
-                words = line.split()
-                """break line into words"""
                 pair = {}
                 pair['src'] = \
                     {'flag':words[0], 'addr':words[1], 'val':words[2]}
                 pair['dest'] = \
                     {'flag':words[3], 'addr':words[4], 'val':words[5]}
-                record = {'record':pair}
+                record = {'record':pair, 'is_in_set': "FALSE"}
                 xtaint_log_list.append(record);
 
     xt_log_fp.close()
-    
+
+def create_buffer_set():
+    """create sets of buffers for each function call.
+    Args:
+        global - xtaint_log_list
+    Return:
+        save in xtaint_buffer_set_list as:
+            [[], [], [],...]
+        each nested list contains members of the set
+    Algorithm:
+        copy xtaint_log_list in to local list
+        scan the local list, repeat until end:
+            look for "RET" records, if found:
+                look for its matched "CALL" record, if found:
+                    save all normal records between
+                    delete the normal records in between, as well as
+                        the paired CALL and RET
+                Otherwise, continue look for next "RET"
+    """
+    local_xt_log_list = list(xtaint_log_list)
+
+    item_index = 0
+    for item in local_xt_log_list:
+        if "RET" in item:
+#             print(item)
+            is_match_found = False
+
+            """scan backwards to look for its matched CALL"""
+            prev_item_index = item_index - 1
+            while prev_item_index >= 0:
+                prev_item = local_xt_log_list[prev_item_index]
+                if "CALL" in prev_item and \
+                        prev_item['CALL']['level'] == item['RET']['level']:
+                    is_match_found = True
+                    break
+                prev_item_index -= 1
+
+            if is_match_found:
+                """if found, add records between to set"""
+#                 print("found a match")
+                buffer_set = [prev_item, prev_item_index, item, item_index]
+                for record in local_xt_log_list[prev_item_index:item_index]:
+                    if "RET" not in record and\
+                            "CALL" not in record and \
+                            record['is_in_set'] == "FALSE":
+                        record['is_in_set'] = "TRUE"
+
+                        """only save if src or dest is memory addr"""
+                        if len(record['record']['src']['addr']) == 8:
+                            buffer_set.append(record['record']['src'])
+                        elif len(record['record']['dest']['addr']) == 8:
+                            buffer_set.append(record['record']['dest'])
+                            
+                xtaint_buffer_set_list.append(buffer_set)
+                
+#             else: print("matched CALL not found")
+            
+        item_index += 1
+        
 def main():
     preprocess()
-    for record in xtaint_log_list:
-        print(record)
+#     for record in xtaint_log_list:
+#         print(record)
+    create_buffer_set()
+    for member in xtaint_buffer_set_list:
+        for record in member:
+            print(record)
  
 if __name__ == '__main__':
     main()
