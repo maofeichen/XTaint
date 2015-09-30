7d476f8e6eacee6f8dbe02cfa54e807b8b1e5ad3
mchen <mchen2016@gmail.com>
2015-09-30 16:31:10 -0400

    taint vir mem - pass args includes taint pattern

--- Decaf1.9-82815/plugins/taint_vir_mem/taint_vir_mem.c
+++ Decaf1.9-82815/plugins/taint_vir_mem/taint_vir_mem.c
@@ -7,8 +7,8 @@
 #include "DECAF_target.h"
 #include "tainting/taintcheck_opt.h"
 
-#define BEGIN_ADDR      0xbffff7cc
-#define TAINT_SZ        4
+//#define BEGIN_ADDR      0xbffff7cc
+//#define TAINT_SZ        4
 
 //basic stub for plugins
 static plugin_interface_t taint_mem_interface;
@@ -17,35 +17,41 @@ static DECAF_Handle mem_opera_handle = DECAF_NULL_HANDLE;
 static int tainted_flag = 0;
 static int taint_addr = 0;
 static int taint_sz = 0;
+static uint8_t taint_pattern = 0;
 
 void do_pass_taint_arg(Monitor *mon, const QDict *qdict)
 {
     uint32_t mem_addr, mem_size;
+    uint8_t pattern;
     if(qdict != NULL){
         mem_addr = qdict_get_int(qdict, "mem_addr");
         mem_size = qdict_get_int(qdict, "mem_size");
-        DECAF_printf("The mem addr: %x and mem_size: %d", mem_addr, mem_size);
+        pattern = qdict_get_int(qdict, "taint_pattern");
+        DECAF_printf("The mem addr: %x and mem_size: %d, taint_pattern: %x\n", \
+                mem_addr, mem_size, pattern);
 
         taint_addr = mem_addr;
         taint_sz = mem_size;
+        taint_pattern = pattern;
     }
 }
 
 /*
  * Handler to implement the command taint_guestOS_mem.
  */
-void do_taint_memory(int addr, int sz){
+void do_taint_memory(int addr, int sz, uint8_t pattern){
 //    tainted_flag = 1;
-    uint8_t taint_mark = 0xff;
+//    uint8_t taint_mark = 0xff;
     uint8_t taint_source[sz];
     if(sz != 0){
-        memset(taint_source, taint_mark, sz);
+        memset(taint_source, pattern, sz);
         if(taintcheck_taint_virtmem(addr, sz, taint_source) != 0){
             DECAF_printf("Fail to taint guest OS memory!\n");
         } else{
             DECAF_printf("Sucessfully to taint guest OS memory!\n");
             taint_addr = 0;
             taint_sz = 0;
+            taint_pattern = 0;
         }
     }
 //    memset(taint_source, taint_mark, TAINT_SZ);
@@ -62,7 +68,7 @@ void do_taint_memory(int addr, int sz){
  */
 static void load_mem_read_callback(DECAF_Callback_Params* param) {
     if(param->mr.vaddr == taint_addr && taint_addr != 0){
-        do_taint_memory(taint_addr, taint_sz);
+        do_taint_memory(taint_addr, taint_sz, taint_pattern);
 //    if(param->mr.vaddr == BEGIN_ADDR){
 //        if(!tainted_flag)
 //            do_taint_memory();
@@ -74,7 +80,7 @@ static void load_mem_read_callback(DECAF_Callback_Params* param) {
 
 static void load_mem_write_callback(DECAF_Callback_Params* param) {
     if(param->mw.vaddr == taint_addr && taint_addr != 0){
-        do_taint_memory(taint_addr, taint_sz);
+        do_taint_memory(taint_addr, taint_sz, taint_pattern);
 //    if(param->mw.vaddr == BEGIN_ADDR){
 //        if(!tainted_flag)
 //            do_taint_memory();
@@ -116,19 +122,19 @@ static void taint_mem_cleanup(void) {
  * Commands supported by the plugin. Included in plugin_cmds.h
  */
 static mon_cmd_t taint_mem_cmds[] = {
-		{
-		    .name = "taint_guestOS_mem",
-		    .args_type = "",
-		    .mhandler.cmd = do_taint_memory,
-		    .params = "no params",
-		    .help = "Taint a specific memory buffer in guest OS"
-		},
+//		{
+//		    .name = "taint_guestOS_mem",
+//		    .args_type = "",
+//		    .mhandler.cmd = do_taint_memory,
+//		    .params = "no params",
+//		    .help = "Taint a specific memory buffer in guest OS"
+//		},
 		{
 		    .name       = "pass_taint_args",
-		    .args_type  = "mem_addr:i,mem_size:i",
+		    .args_type  = "mem_addr:i,mem_size:i,taint_pattern:i",
 		    .mhandler.cmd   = do_pass_taint_arg,
-		    .params     = "mem_addr mem_size",
-		    .help       = "pass the begin addr and size of tainting memory"
+		    .params     = "mem_addr mem_size taint_pattern",
+		    .help       = "pass the begin addr,size and pattern tainting memory"
 		},
 		{ NULL, NULL, },
 };
